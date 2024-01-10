61f3ce01c60062708380280739989e358cc924da
Niall Barber <niall.barber@olioex.com>
2024-01-10 19:35:50 +0000

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Niall Barber on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
